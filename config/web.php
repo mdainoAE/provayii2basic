e2d8af4ca479eda34c8d3c6cbab3da41d7bd056e
mdainoAE <marcello.dainotti@gmail.com>
2023-07-02 18:04:48 +0200

    Prime modifiche web.php views\site\index.php

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'La mia App BASIC',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
